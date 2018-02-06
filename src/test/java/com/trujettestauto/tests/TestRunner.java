0de179f4f3318c37386e17102277c5a8fb2bd8ef
nraobodduri <nraobodduri@abilitynetwork.com>
2018-02-06 13:29:41 +0530

    updated code for extent reports

--- src/test/java/com/trujettestauto/tests/TestRunner.java
+++ src/test/java/com/trujettestauto/tests/TestRunner.java
@@ -1,6 +1,5 @@
 package com.trujettestauto.tests;
 
-import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,20 +16,33 @@ import cucumber.api.Scenario;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true, features = { "classpath:feature_files_trujet/" }, glue = {
-"com.trujettestauto.tests" }, tags = "@Login", plugin = { "html:target/cucumber-html-report",
-        "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
-        "usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml" })
+@CucumberOptions(
+	monochrome = true, 
+	features = { "classpath:feature_files_trujet/" }, 
+	glue = {"com.trujettestauto.tests" }, 
+	tags = "@Login", 
+	format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"},
+	plugin = {"com.cucumber.listener.ExtentCucumberFormatter"})
 public class TestRunner {
 
 	@SuppressWarnings("unchecked")
 	@BeforeClass
 	public static void setup() {
-	
+		// Initiates the extent report and generates the output in the
+        // output/Run_<unique timestamp>/report.html file by default.
+        ExtentCucumberFormatter.initiateExtentCucumberFormatter();
+
+        // User can add the system information as follows
+        ExtentCucumberFormatter.addSystemInfo("Selenium version", "v2.53.0");
+
+        // Also you can add system information using a hash map
+        final Map systemInfo = new HashMap();
+        systemInfo.put("Cucumber version", "v1.2.4");
+        systemInfo.put("Extent Cucumber Reporter version", "v1.1.1");
+        ExtentCucumberFormatter.addSystemInfo(systemInfo);
 	}
 
 	public static void embedScreenshot(Scenario scenario, WebDriver driver) {
-
 		try {
 			byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
 			scenario.embed(screenshot, "image/png");
